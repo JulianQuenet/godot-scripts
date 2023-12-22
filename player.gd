d0999dd2a23b58de99191713856752385df4d4e3
Julian Quenet <125870227+JulianQuenet@users.noreply.github.com>
2023-12-22 18:59:29 +0200

    Update player.gd

--- player.gd
+++ player.gd
@@ -160,11 +160,7 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		sliding = false
+	
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
